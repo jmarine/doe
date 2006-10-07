67397cde4a65939a39409f382ee39510ba2861ce
jmarine <jmarine@86cbd5c9-70c2-6923-a755-8718247db402>
2006-10-07 18:51:42 +0000

    Display related entities in tables to show custom properties (only with "1:*" and "*:*" relations)

--- trunk/doe4ejb3/src/org/doe4ejb3/gui/EditorFactory.java
+++ trunk/doe4ejb3/src/org/doe4ejb3/gui/EditorFactory.java
@@ -7,6 +7,8 @@
 
 package org.doe4ejb3.gui;
 
+import com.sun.imageio.plugins.common.I18N;
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
@@ -14,9 +16,11 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.lang.reflect.InvocationTargetException;
 
+import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
 
 import java.util.ArrayList;
@@ -33,6 +37,7 @@ import javax.swing.JComboBox;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JScrollPane;
@@ -40,6 +45,10 @@ import javax.swing.JOptionPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.SpinnerDateModel;
 import javax.swing.DefaultListModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.event.EventListenerList;
 import javax.swing.text.JTextComponent;
 
 
@@ -100,11 +109,9 @@ public class EditorFactory {
                 // OneToMany || ManyToMany
                 try {
                     System.out.println("EditorFactory: OneToMany or ManyToMany!!!");
-                    
-                    final DefaultListModel listModel = new DefaultListModel();
-                    Method modelGetter = listModel.getClass().getMethod("toArray");
-                    binder = new JComponentDataBinder(listModel, modelGetter, editor, property);
-                    comp = getEditorForMultivaluedProperty(memberClass, property, listModel);
+                    JComponentDataBinder binderOutParam[] = new JComponentDataBinder[1];
+                    comp = getEditorForMultivaluedProperty(memberClass, property, editor, defaultLength, binderOutParam);
+                    binder = binderOutParam[0];
 
                 } catch(Exception ex) {
                     System.out.println("Error loading property: " + ex.getMessage());
@@ -208,7 +215,6 @@ public class EditorFactory {
                         comp = textField;
                     } else {
                         textField = new javax.swing.JTextPane();
-                        // textField.setMinimumSize(new java.awt.Dimension(defaultLength * 10, 80));
                         textField.setPreferredSize(new java.awt.Dimension(defaultLength * 10, 80));
                         compGetter = textField.getClass().getMethod("getText");
                         binder = new JComponentDataBinder(textField, compGetter, editor, property);                        
@@ -249,9 +255,13 @@ public class EditorFactory {
      * Setup an editor for a multi-valued property 
      * TODO: allow drag and drop operations
      */
-    private static JComponent getEditorForMultivaluedProperty(final Class memberClass, final Property property, final DefaultListModel listModel) throws InvocationTargetException, IllegalAccessException {
+    private static JComponent getEditorForMultivaluedProperty(final Class memberClass, final Property property, final java.beans.PropertyEditor editor, int defaultLength, JComponentDataBinder binderOutParam[]) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException, Exception {
         final JPanel panel = new JPanel();
-        final List allValues = JPAUtils.findAllEntities(memberClass);
+        final DefaultListModel listModel = new DefaultListModel();
+        
+        // configure data binder
+        Method modelGetter = listModel.getClass().getMethod("toArray");
+        binderOutParam[0] = new JComponentDataBinder(listModel, modelGetter, editor, property);
 
         // populate listmodel with property's values
         Collection values = (Collection)property.getValue();
@@ -262,35 +272,54 @@ public class EditorFactory {
             listModel.addElement(valueToSelect);
         }
 
-        // configure JList component
-        final JList jList = new JList();
-        jList.setVisibleRowCount(6);
-        jList.setSelectionMode(javax.swing.ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        jList.setModel(listModel);
+        // choose best UI control to display item list
+        JScrollPane scrollableItems = null;
+        ListSelectionModel listSelectionModel = null;
+        ObjectPropertyTableModel objectPropertyTableModel = new ObjectPropertyTableModel(memberClass, listModel);
+        if(objectPropertyTableModel.getColumnCount() > 0) {
+            JTable jTable = new JTable(objectPropertyTableModel);
+            jTable.setSelectionMode(javax.swing.ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+            jTable.setRowSelectionAllowed(true);
+            listSelectionModel = jTable.getSelectionModel();
+            scrollableItems = new JScrollPane(jTable);
+        } else {
+            JList jList = new JList();
+            jList.setVisibleRowCount(6);
+            jList.setSelectionMode(javax.swing.ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+            jList.setModel(listModel);
+            listSelectionModel = jList.getSelectionModel();
+            scrollableItems = new JScrollPane(jList);
+        }
+      
         
         // configure "add" button
         JButton btnAddItem = new JButton("Add");
         btnAddItem.addActionListener(new ActionListener() {
            public void actionPerformed(ActionEvent evt)  {
-               Object newItem = JOptionPane.showInternalInputDialog(panel, "Select new item:", "Add new item", JOptionPane.QUESTION_MESSAGE, null, allValues.toArray(), null);
+               List allValues = JPAUtils.findAllEntities(memberClass);
+               Object newItem = JOptionPane.showInternalInputDialog(panel, "Select new item:", "Add new " + I18n.getEntityName(memberClass), JOptionPane.QUESTION_MESSAGE, null, allValues.toArray(), null);
                if(newItem != null) {
-                  if(!listModel.contains(newItem)) {
-                     listModel.addElement(newItem);
+                  // FIXME: caution with duplicated relations and "Set" collection types.
+                  if(!listModel.contains(newItem)) {  
+                      listModel.addElement(newItem);
+                  } else {
+                      JOptionPane.showInternalMessageDialog(panel, "Selected item already exists!", "Error:", JOptionPane.ERROR_MESSAGE);
                   }
                }
            }
         });
 
         // configure "delete" button
+        final ListSelectionModel listSelectionModelFinal = listSelectionModel;
         JButton btnDeleteItem = new JButton("Delete");
         btnDeleteItem.addActionListener(new ActionListener() {
            public void actionPerformed(ActionEvent evt)  {
-               int indexesToRemove[] = jList.getSelectedIndices();
-               System.out.println("EditorFactory: removing selected items: " + indexesToRemove);
-               if(indexesToRemove != null) {
-                   for(int i = 0; i < indexesToRemove.length; i++) {
-                       System.out.println("EditorFactory: removing item at: " + indexesToRemove[i]);
-                       listModel.removeElementAt(indexesToRemove[i]);
+               if(!listSelectionModelFinal.isSelectionEmpty()) {
+                   for(int index = listSelectionModelFinal.getMaxSelectionIndex(); index >= listSelectionModelFinal.getMinSelectionIndex(); index--) {
+                       if(listSelectionModelFinal.isSelectedIndex(index)) {
+                           System.out.println("EditorFactory: removing selected index: " + index);
+                           listModel.removeElementAt(index);
+                       }
                    }
                }
            }
@@ -298,10 +327,15 @@ public class EditorFactory {
         
         
         // configure panel layout
-        panel.setLayout(new GridBagLayout());
-        panel.add(new JScrollPane(jList), gbcList);
-        panel.add(btnAddItem, gbcButton);
-        panel.add(btnDeleteItem, gbcButton);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+        buttonPanel.add(btnAddItem, gbcButton);
+        buttonPanel.add(btnDeleteItem, gbcButton);
+        
+        panel.setPreferredSize(new java.awt.Dimension(450, 180));        
+        panel.setLayout(new BorderLayout());
+        panel.add("Center", scrollableItems);
+        panel.add("South", buttonPanel);
         
         return panel;
     }
@@ -313,4 +347,178 @@ public class EditorFactory {
     /** Private constants to layout buttons */
     private final static GridBagConstraints gbcButton = new GridBagConstraints(GridBagConstraints.RELATIVE,1, 1, 1, 1.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0,0,5,3), 0,0);
     
+}
+
+
+class ObjectPropertyTableModel implements javax.swing.table.TableModel, javax.swing.event.ListDataListener
+{
+    private Class itemClass;
+    private DefaultListModel listModel;
+    private ArrayList<Member> columnMembers;
+    private ArrayList<org.doe4ejb3.annotation.PropertyDescriptor> columnPropertyDescriptors;
+    private EventListenerList listenerList;
+    
+    public ObjectPropertyTableModel(Class itemClass, DefaultListModel listModel) throws Exception
+    {
+        this.itemClass = itemClass;
+        this.listModel = listModel;
+        this.listModel.addListDataListener(this);
+        
+        this.columnMembers = new ArrayList<Member>();
+        this.columnPropertyDescriptors = new ArrayList<org.doe4ejb3.annotation.PropertyDescriptor>();
+        this.listenerList = new EventListenerList();
+
+        // TODO: order by index
+        System.out.println("ObjectPropertyDescriptorTableModel: Scan columns...");
+        for(Field field : itemClass.getFields()) {
+            org.doe4ejb3.annotation.PropertyDescriptor pd = field.getAnnotation(org.doe4ejb3.annotation.PropertyDescriptor.class);
+            if( (pd != null) && (pd.showInLists()) ) {
+                columnMembers.add(field);
+                columnPropertyDescriptors.add(pd);
+                System.out.println("ObjectPropertyDescriptorTableModel: found column: " + field.getName());
+            }
+        }
+
+        java.beans.BeanInfo bi = java.beans.Introspector.getBeanInfo(itemClass);
+        for(java.beans.PropertyDescriptor bpd : bi.getPropertyDescriptors()) {
+            Method method = bpd.getReadMethod();
+            if(method != null) {
+                org.doe4ejb3.annotation.PropertyDescriptor pd = method.getAnnotation(org.doe4ejb3.annotation.PropertyDescriptor.class);
+                if( (pd != null) && (pd.showInLists()) ) {
+                    columnMembers.add(method);
+                    columnPropertyDescriptors.add(pd);
+                    System.out.println("ObjectPropertyDescriptorTableModel: found column: " + method.getName());
+                }
+            }
+        }
+        System.out.println("ObjectPropertyDescriptorTableModel: Scan done.");        
+
+    }
+
+    public int getRowCount() {
+        int rows = listModel.getSize();
+        System.out.println("ObjectPropertyDescriptorTableModel: RowCount =" + rows);
+        return rows;
+    }
+
+    public int getColumnCount() {
+        int cols = columnMembers.size();
+        System.out.println("ObjectPropertyDescriptorTableModel: Num columns =" + cols);
+        return cols;
+    }
+
+    public String getColumnName(int columnIndex) {
+        String columnName = "";
+        try {
+            if( (columnIndex >= 0) && (columnIndex < columnPropertyDescriptors.size()) ) {
+                org.doe4ejb3.annotation.PropertyDescriptor pd = columnPropertyDescriptors.get(columnIndex);
+                if((pd != null) && (pd.displayName() != null) && (pd.displayName().length() > 0) ) {
+                    columnName = pd.displayName();
+                } else {
+                    Member member = columnMembers.get(columnIndex);
+                    if(member != null) {
+                        columnName = member.getName();
+                        if(columnName.startsWith("set")) columnName = columnName.substring(3);
+                        else if(columnName.startsWith("get")) columnName = columnName.substring(3);
+                        else if(columnName.startsWith("is")) columnName = columnName.substring(2);
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            columnName = "N/A";
+        }
+        return columnName;
+    }
+
+    public Class getColumnClass(int columnIndex) {
+        if( (columnIndex >= 0) && (columnIndex < columnMembers.size()) ) {
+            Member member = columnMembers.get(columnIndex);
+            if(member != null) {
+                if(member instanceof Field) return ((Field)member).getType();
+                else if(member instanceof Method) return ((Method)member).getReturnType();
+            }
+        }
+        return String.class;
+    }
+
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Object obj = listModel.getElementAt(rowIndex);
+        try {
+            Object retval = obj.toString();
+            if( (columnIndex >= 0) && (columnIndex < columnMembers.size()) ) {
+                Member member = columnMembers.get(columnIndex);
+                if(member != null) {
+                    if(member instanceof Field) retval = ((Field)member).get(obj);
+                    else if(member instanceof Method) retval = ((Method)member).invoke(obj);
+                }
+            }        
+            System.out.println("Object " + rowIndex + ", property " + columnIndex + " = " + retval);
+            return retval;
+        } catch(Exception ex) {
+            return "N/A";
+        }
+    }
+
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        // not needed
+    }
+
+    /**
+     * Adds a listener to the list that's notified each time a change
+     * to the data model occurs.
+     *
+     * @param   l               the TableModelListener
+     */
+    public void addTableModelListener(TableModelListener l) {
+        System.out.println("EditorFactory: addTableModelListener.");
+        listenerList.add(TableModelListener.class, l);
+    }
+
+    /**
+     * Removes a listener from the list that's notified each time a
+     * change to the data model occurs.
+     *
+     * @param   l               the TableModelListener
+     */
+    public void removeTableModelListener(TableModelListener l) {
+        listenerList.remove(TableModelListener.class, l);
+    }
+
+    public void intervalAdded(ListDataEvent e) {
+        fireChangeEvents(e);
+    }
+
+    public void intervalRemoved(ListDataEvent e) {
+        System.out.println("EditorFactory: intervalRemoved: " + e);
+        fireChangeEvents(e);
+    }
+
+    public void contentsChanged(ListDataEvent e) {
+        fireChangeEvents(e);
+    }
+    
+    private void fireChangeEvents(ListDataEvent e)
+    {
+        int tableModelEventType = TableModelEvent.UPDATE;
+        switch(e.getType()) {
+            case ListDataEvent.INTERVAL_ADDED: tableModelEventType = TableModelEvent.INSERT; break;
+            case ListDataEvent.INTERVAL_REMOVED: tableModelEventType = TableModelEvent.DELETE; break;
+            case ListDataEvent.CONTENTS_CHANGED: tableModelEventType = TableModelEvent.UPDATE; break;
+        }
+        
+        TableModelEvent event = new TableModelEvent(this, e.getIndex0(), e.getIndex1(), javax.swing.event.TableModelEvent.ALL_COLUMNS, tableModelEventType);
+        // Process the listeners last to first, notifying
+        // those that are interested in this event
+        Object[] listeners = listenerList.getListenerList();
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==TableModelListener.class) {
+                System.out.println("EditorFactory: notification of tableChanged to: " + listeners[i+1]);
+                ((TableModelListener)listeners[i+1]).tableChanged(event);
+            }
+        }
+    }
 }
