a9b4ed7015e9bcc987d97e66f7c99768cc64b64a
Jordi Mariné Fort <jmarine@users.noreply.github.com>
2007-08-24 16:44:55 +0000

    Addition of some comments.

--- doe4ejb3/src/org/doe4ejb3/annotation/EntityDescriptor.java
+++ doe4ejb3/src/org/doe4ejb3/annotation/EntityDescriptor.java
@@ -1,6 +1,8 @@
-/*
+/**
  * EntityDescriptor.java
  *
+ * Use this annotation to configure EJB3 entity bean UI, visiblity and icon.
+ * 
  * Created on 29 July 2006, 21:55
  * @author Jordi Marine Fort
  */
@@ -12,11 +14,27 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD}) 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface EntityDescriptor {
+public @interface EntityDescriptor 
+{
+    /**
+     * To display the EJB3 entity bean details with a customized editor.
+     * The class must have to implement the interface EntityEditorInterface.
+     */
     String editorClassName() default "org.doe4ejb3.gui.EntityEditorImpl";
-    String iconPath() default "";
+
+    /**
+     * To hide the EJB3 entity bean from menús and navigator panel.
+     * (entities are only visible when used as child relations).
+     */
     boolean hidden() default false;
+
+    
+    /**
+     * To configure the icon that should be displayed with the EJB3 entity name 
+     * in the navigator panel and internal frames.
+     */
+    String iconPath() default "";
+    
 }
