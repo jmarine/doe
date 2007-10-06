8132de6be0d6a862c6d624e4a6d81e10ae68b54f
Jordi Mariné Fort <jmarine@users.noreply.github.com>
2007-10-06 17:05:27 +0000

    Move scrollbar on  focus of all subcomponents.

--- doe4ejb3/src/org/doe4ejb3/gui/JAutoScrollPaneOnComponentFocus.java
+++ doe4ejb3/src/org/doe4ejb3/gui/JAutoScrollPaneOnComponentFocus.java
@@ -20,49 +20,80 @@ import java.awt.event.FocusListener;
 
 import javax.swing.JScrollPane;
 import javax.swing.JViewport;
+import javax.swing.SwingUtilities;
 
-/**
- * JAutoScrollPaneOnComponentFocus.java
- *
- * Created on 02 September 2006, 19:20
- * @author Jordi Marine Fort
- */
+import org.doe4ejb3.util.DOEUtils;
+
+
 public class JAutoScrollPaneOnComponentFocus extends JScrollPane implements ContainerListener, FocusListener
 {
     private static final int PADY = 3;
-
+    
     /**
      * Creates a new instance of JAutoScrollPaneOnComponentFocus
      */
     public JAutoScrollPaneOnComponentFocus(Container container, int vsbPolicy, int hsbPolicy) 
     {
         super(container, vsbPolicy, hsbPolicy);
-        container.addContainerListener(this);
-        for(Component component : container.getComponents()) {
-            component.addFocusListener(this);
-        }
+        addFocusListeners(container);
     }
 
+    
     public void componentAdded(ContainerEvent event) 
     {
         Component component = event.getComponent();
-        component.addFocusListener(this);
+        addFocusListeners(component);
     }
 
+    
     public void componentRemoved(ContainerEvent event) 
     {
         Component component = event.getComponent();
-        component.removeFocusListener(this);
+        removeFocusListeners(component);
     }
+    
+    
+    private void addFocusListeners(Component component)
+    {
+        component.addFocusListener(this);
+        if(component instanceof Container) {
+            Container container = (Container)component;
+            container.addContainerListener(this);
+            for(Component contained : container.getComponents()) {
+                addFocusListeners(contained);
+            }
+        }
+    }
+
+    
+    private void removeFocusListeners(Component component)
+    {
+        component.removeFocusListener(this);
+        if(component instanceof Container) {
+            Container container = (Container)component;
+            container.removeContainerListener(this);
+            for(Component contained : container.getComponents()) {
+                removeFocusListeners(contained);
+            }
+        }
+    }    
+    
 
     public void focusGained(FocusEvent event) 
     {
-        Component c = (Component)event.getSource();
-        JViewport vp = this.getViewport();
-        Point vpp = vp.getViewPosition();
+        Component c    = (Component)event.getSource();
+        JViewport vp   = this.getViewport();
+        Point     vpp  = vp.getViewPosition();
+        Component view = getViewport().getView();
+
         Rectangle rect = c.getBounds();
+        for(Component tmp = c.getParent(); tmp != view; tmp = tmp.getParent()) {
+            Point p = tmp.getLocation();
+            rect.x = rect.x + p.x;
+            rect.y = rect.y + p.y;
+        }
         
-        vpp.x = 0;
+        // vpp.x = 0;
         if(vpp.y > rect.y) {
             vpp.y = rect.y - PADY;
         } else if(vpp.y+vp.getExtentSize().height < rect.y+rect.height+PADY) {
