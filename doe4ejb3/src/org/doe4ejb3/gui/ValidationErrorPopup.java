4cfe8ffacf04c1d807b311538abd1b875719e609
Jordi Mariné Fort <jmarine@users.noreply.github.com>
2007-10-06 17:11:21 +0000

    Move scrollbars to show component with invalid content, and adjust popup position at the bottom of it.

--- doe4ejb3/src/org/doe4ejb3/gui/ValidationErrorPopup.java
+++ doe4ejb3/src/org/doe4ejb3/gui/ValidationErrorPopup.java
@@ -13,35 +13,25 @@ import javax.swing.*;
 import org.doe4ejb3.util.DOEUtils;
 
 
-public class ValidationErrorPopup implements ActionListener
+public class ValidationErrorPopup
 {
     private static final Color POPUP_COLOR = new Color(243, 255, 159);
 
-    private JDialog popup;
-    
-    
-    private JDialog getPopup()
-    {
-        if(popup == null) {
-            popup = new JDialog(DOEUtils.getWindowManager().getMainWindow(), true);
-        }
-        return popup;
-    }
-    
-    
-    public static void showErrorPopup(JComponent c, String message) {
-        ValidationErrorPopup popupListener = new ValidationErrorPopup();
-	Color color = c.getBackground();
-        c.setBackground(Color.PINK);
-        c.requestFocus();  // move scroll for component visibility
+    public static void showErrorPopup(final JComponent c, final String message) {
+        final Color oldColor = c.getBackground();
+        final JDialog popup = new JDialog(DOEUtils.getWindowManager().getMainWindow(), true);
         
         JLabel messageLabel = new JLabel(message);
-
         JButton image = new JButton(new javax.swing.ImageIcon(ValidationErrorPopup.class.getResource("/org/doe4ejb3/gui/resources/cancel.png")));
         image.setContentAreaFilled(false);
         image.setMargin(new java.awt.Insets(0, 0, 0, 0));
         image.setBorderPainted(false);
-        image.addActionListener(popupListener);
+        image.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                popup.setVisible(false);
+                c.setBackground(oldColor);
+            }            
+        });
 
         JPanel container = new JPanel();
         container.setLayout(new FlowLayout());
@@ -50,31 +40,30 @@ public class ValidationErrorPopup implements ActionListener
         container.add(image);
         container.add(messageLabel);
 
-        JDialog popup = popupListener.getPopup();
         popup.getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT, 0,0));
         popup.setUndecorated(true);
         popup.getContentPane().setBackground(POPUP_COLOR);
         popup.getContentPane().add(container);
-        popup.setFocusableWindowState(false);
-
-
-        popup.setSize(0, 0);
-        popup.setLocationRelativeTo(c);
-        Point point = popup.getLocation();
-        Dimension cDim = c.getSize();
-        popup.setLocation(point.x-(int)cDim.getWidth()/2,
-                          point.y+(int)cDim.getHeight()/2);
+        popup.setFocusableWindowState(false);        
         
-        popup.pack();
-        popup.setVisible(true);
-        
-        c.setBackground(color);
+        c.setBackground(Color.PINK);
+        c.requestFocus();                            // may move scrollbars for component visibility
+        SwingUtilities.invokeLater(new Runnable() {  // so, delay to open popup on new position
+            public void run() {
+                popup.setSize(0, 0);
+                popup.setLocationRelativeTo(c);
+                Point point = popup.getLocation();
+                Dimension cDim = c.getSize();
+                popup.setLocation(point.x-(int)cDim.getWidth()/2,
+                                  point.y+(int)cDim.getHeight()/2);
 
+                popup.pack();
+                popup.setVisible(true);
+            }
+        });
     }
         
     
-    public void actionPerformed(ActionEvent e) {
-        popup.setVisible(false);
-    }
+
 
 }
