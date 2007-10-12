8fe1e837645d0505b3f6414cd4140f8129399a18
Jordi Mariné Fort <jmarine@users.noreply.github.com>
2007-10-12 17:04:35 +0000

    Preparations for i18n.

--- doe4ejb3/src/org/doe4ejb3/gui/I18n.java
+++ doe4ejb3/src/org/doe4ejb3/gui/I18n.java
@@ -7,21 +7,34 @@
 
 package org.doe4ejb3.gui;
 
+import org.jdesktop.application.ResourceMap;
+
 
 public class I18n 
 {
+    private static ResourceMap doeResourceMap = org.jdesktop.application.Application.getInstance().getContext().getResourceMap(DomainObjectExplorer.class);
     
-    /** Creates a new instance of I18n */
-    public static String getLiteral(String key) 
+
+    public static String getLiteral(String key, Object... args)
     {
-        // TODO: get localized message
-        return key;
+        String value = doeResourceMap.getString(key, args);
+        if(value == null) value = key;
+        return value;
     }
+    
+    public static String getLiteral(Class baseClass, String key, Object... args) 
+    {
+        ResourceMap resourceMap = org.jdesktop.application.Application.getInstance().getContext().getResourceMap(baseClass);
+        String value = resourceMap.getString(key, args);
+        if(value == null) value = key;
+        return value;
+    }
+    
     
     public static String getEntityName(Class entity)
     {
         String name = entity.getName();
-        String msg = I18n.getLiteral(name);
+        String msg = I18n.getLiteral(entity, name);
         if( (msg == null) || (name.equals(msg)) ) {
             int pos = msg.lastIndexOf(".");
             msg = name.substring(pos+1);
